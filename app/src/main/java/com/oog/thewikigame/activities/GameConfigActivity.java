470f735a6f7c8f061637b22fad7151c169c46131
OmerGez9001 <omergez72@gmail.com>
2021-07-10 20:58:22 +0300

    GameConfig Expandable
    [+] Created Expandable on GameConfig
    [+] Added list_item_two_line_text_switch.xml
    [+] Added ListItemTwoLineTextSwitchModel

--- app/src/main/java/com/oog/thewikigame/activities/GameConfigActivity.java
+++ app/src/main/java/com/oog/thewikigame/activities/GameConfigActivity.java
@@ -2,29 +2,30 @@ package com.oog.thewikigame.activities;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import com.android.volley.NetworkResponse;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.oog.thewikigame.R;
 import com.oog.thewikigame.databinding.ActivityGameConfigBinding;
 import com.oog.thewikigame.models.IconButtonModel;
 import com.oog.thewikigame.models.TwoLineSwitchModel;
-import com.oog.thewikigame.utilities.LogTag;
-import com.oog.thewikigame.utilities.Logger;
 
 import java.util.concurrent.CompletableFuture;
 
 public class GameConfigActivity extends AppCompatActivity {
+
+    LinearLayout expandableView;
+    TextView textView;
+    CardView cardView;
+
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -32,6 +33,21 @@ public class GameConfigActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         ActivityGameConfigBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_game_config);
+        binding.showconfig.setOnClickListener((v)->{
+            if (expandableView.getVisibility() == View.GONE){
+                TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                expandableView.setVisibility(View.VISIBLE);
+            }
+            else{
+                TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                expandableView.setVisibility(View.GONE);
+            }
+        });
+
+        expandableView = findViewById(R.id.expandable_view);
+        textView = findViewById(R.id.showconfig);
+        cardView = findViewById(R.id.cardview_expandable);
+
 
         binding.setStartArticleString("Son Goku");
         binding.setStartArticleIconButtonModel(new IconButtonModel(this, R.drawable.ic_baseline_refresh_24, v -> {
@@ -39,7 +55,7 @@ public class GameConfigActivity extends AppCompatActivity {
             binding.setStartArticleString(this.getString(R.string.text_loading));
             //TODO: Add web view integration.
             //TODO: delete this simulation.
-            CompletableFuture.runAsync(()->{
+            CompletableFuture.runAsync(() -> {
                 binding.setStartArticleString(this.getString(R.string.text_loading));
                 try {
                     Thread.sleep(3000);
@@ -50,13 +66,11 @@ public class GameConfigActivity extends AppCompatActivity {
             });
         }));
 
-
-        binding.setTimeLimitTwoLineSwitchModel(new TwoLineSwitchModel("Time Limit","This will set a time limit to finish the game.") {
+        binding.setTimeLimitTwoLineSwitchModel(new TwoLineSwitchModel("Time Limit", "This will set a time limit to finish the game.") {
             @Override
             public void onToggle(boolean checked) {
                 //TODO: Add time limit functionality.
             }
         });
-
     }
 }
